592b0a561e9e593c36f13544129acbab499f3ba3
Ameen Ahmed <kid_1194@hotmail.com>
2022-08-19 21:33:27 +0300

    Initial release

---
+++ frappe_better_numerical_controls/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+    return [
+        {
+            "module_name": "Frappe Better Numerical Controls",
+            "color": "blue",
+            "icon": "octicon octicon-plus",
+            "type": "module",
+            "label": _("Frappe Better Numerical Controls")
+        }
+    ]
