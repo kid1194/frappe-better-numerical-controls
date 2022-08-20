c63df19a4b4d4fddf9b92de7c793307b4d6dd1a7
Ameen Ahmed <kid_1194@hotmail.com>
2022-08-20 16:45:28 +0300

    Initial release update

--- frappe_better_numerical_controls/public/js/better_float.bundle.js
+++ frappe_better_numerical_controls/public/js/better_float.bundle.js
@@ -1,5 +1,4 @@
 import {
-    isVal,
     isDataObject
 } from './utils/check.js';
 
@@ -13,15 +12,32 @@ frappe.ui.form.ControlFloat = class ControlFloat extends frappe.ui.form.ControlF
         if (isDataObject(this.df.options)) {
             let opts = this.df.options,
             me = this;
-            ['min', 'max'].forEach(function(k) {
-                if (isVal(opts[k])) me['_' + k] = flt(opts[k]);
+            ['min', 'max', 'divisible_by'].forEach(function(k) {
+                if (opts[k] != null) me['_' + k] = flt(opts[k]);
+            });
+            if (!this.frm) return;
+            ['min_field', 'max_field'].forEach(function(k) {
+                if (opts[k] != null) me['_' + k] = String(opts[k]);
             });
         }
     }
     parse(value) {
         value = super.parse(value);
-        if (isVal(this._min) && this._min > value) return this._min;
-        if (isVal(this._max) && this._max < value) return this._max;
+        if (this._min != null && this._min > value) return this._min;
+        if (this._max != null && this._max < value) return this._max;
+        if (this._divisible_by != null && value % this._divisible_by !== 0) {
+            return Math.round(value / this._divisible_by) * this._divisible_by;
+        }
+        if (this._min_field) {
+            let field = this.frm.get_field(this._min_field),
+            field_val = field ? flt(field.get_value()) : value;
+            if (field_val > value) return field_val;
+        }
+        if (this._max_field) {
+            let field = this.frm.get_field(this._max_field),
+            field_val = field ? flt(field.get_value()) : value;
+            if (field_val < value) return field_val;
+        }
         return value;
     }
 };
